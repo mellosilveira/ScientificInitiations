24e5dc2a3e164a871737adea1ca487d14830ba08
Bruno Mello <brunomellosilveira1@gmail.com>
2020-05-21 13:31:46 -0300

    Added solutions, fixed Model.Constants and fixed poster.

--- IcVibracoes.Core/Models/Constants.cs
+++ IcVibracoes.Core/Models/Constants.cs
@@ -29,16 +29,6 @@
         public const int DegreesFreedom = 2;
 
         /// <summary>
-        /// Constant used in numerical methods.
-        /// </summary>
-        public const double Beta = 1 / 4;
-
-        /// <summary>
-        /// Constant used in numerical methods.
-        /// </summary>
-        public const double Gama = 1 / 2;
-
-        /// <summary>
         /// Proportionality constant used in the damping calculate. 
         /// It is multiplied by mass matrix.
         /// </summary>
