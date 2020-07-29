e16455df5359bae5755bec57cd48654a51a535e0
Meello <bruno.silveira@stone.com.br>
2020-07-29 13:47:53 -0300

    Added unit tests.

---
+++ IcVibracoes.Test/Core/Calculator/GeometricProperties/Rectangular/RectangularGeometricPropertyTest.cs
@@ -0,0 +1,52 @@
+﻿using IcVibracoes.Common.Profiles;
+using IcVibracoes.Core.Calculator.GeometricProperties.Rectangular;
+using IcVibracoes.Test.Helper;
+using Xunit;
+
+namespace IcVibracoes.Test.Core.Calculator.GeometricProperties.Rectangular
+{
+    public class RectangularGeometricPropertyTest : GeometricPropertyTest<RectangularProfile>
+    {
+        public RectangularGeometricPropertyTest()
+        {
+            base._calculator = new RectangularGeometricProperty();
+
+            base._beamProfileWithThickness = GeometricPropertyModel.RectangularBeamProfileWithThickness;
+
+            base._beamProfileWithoutThickness = GeometricPropertyModel.RectangularBeamProfileWithoutThickness;
+
+            base._piezoelectricProfile = GeometricPropertyModel.RectangularPiezoelectricProfile;
+
+            base._areaWithThickness = GeometricPropertyModel.RectangularAreaWithThickness;
+            base._areaWithoutThickness = GeometricPropertyModel.RectangularAreaWithoutThickness;
+            base._piezoelectricArea = GeometricPropertyModel.RectangularPiezoelectricArea;
+
+            base._momentOfInertiaWithThickness = GeometricPropertyModel.RectangularMomentOfInertiaWithThickness;
+            base._momentOfInertiaWithoutThickness = GeometricPropertyModel.RectangularMomentOfInertiaWithoutThickness;
+            base._piezoelectricMomentOfInertia = GeometricPropertyModel.RectangularPiezoelectricMomentOfInertia;
+
+            base._areaPrecision = GeometricPropertyModel.RectangularAreaPrecision;
+            base._momentOfInertiaPrecision = GeometricPropertyModel.RectangularMomentOfInertiaPrecision;
+        }
+
+        [Fact(DisplayName = @"Feature: CalculatePiezoelectricArea | When: Execute. | Given: Valid parameters. | Should: Calculate correctly.")]
+        public async void CalculatePiezoelectricArea_Should_CalculateCorrectly()
+        {
+            // Arrange
+            var result = await base._calculator.CalculatePiezoelectricArea(this._piezoelectricProfile, this._numberOfElements, this._elementsWithPiezoelectric, this._numberOfPiezoelectricPerElement).ConfigureAwait(false);
+
+            // Assert
+            result.ShouldBeBeApproximately(this._piezoelectricArea, this._areaPrecision);
+        }
+
+        [Fact(DisplayName = @"Feature: CalculatePiezoelectricMomentOfInertia | When: Execute. | Given: Valid parameters. | Should: Calculate correctly.")]
+        public async void CalculatePiezoelectricMomentOfInertia_Should_CalculateCorrectly()
+        {
+            // Arrange
+            var result = await base._calculator.CalculatePiezoelectricMomentOfInertia(this._piezoelectricProfile, this._beamProfileWithThickness, this._numberOfElements, this._elementsWithPiezoelectric, this._numberOfPiezoelectricPerElement).ConfigureAwait(false);
+
+            // Assert
+            result.ShouldBeBeApproximately(this._piezoelectricMomentOfInertia, this._momentOfInertiaPrecision);
+        }
+    }
+}
---
+++ IcVibracoes.Test/Helper/RequestStub.cs
@@ -0,0 +1,86 @@
+﻿using IcVibracoes.Common.Classes;
+using IcVibracoes.Common.Profiles;
+using IcVibracoes.DataContracts.FiniteElement.Beam;
+using System.Collections.Generic;
+
+namespace IcVibracoes.Test.Helper
+{
+    public static class RequestStub
+    {
+        public static BeamRequest<CircularProfile> CircularBeam
+            => new BeamRequest<CircularProfile>
+            {
+                AngularFrequencyStep = 1,
+                Author = "Darth Vader",
+                Fastenings = new List<Fastening>
+                {
+                    new Fastening
+                    {
+                        NodePosition = 0,
+                        Type = "Pinned"
+                    },
+                    new Fastening
+                    {
+                        NodePosition = 2,
+                        Type = "Pinned"
+                    }
+                },
+                Forces = new List<Force>
+                {
+                    new Force
+                    {
+                        NodePosition = 1,
+                        Value = 10
+                    }
+                },
+                ForceType = "Harmonic",
+                InitialAngularFrequency = 0,
+                FinalAngularFrequency = 10,
+                Length = 1,
+                Material = "Aluminum",
+                NumberOfElements = 2,
+                NumericalMethod = "Newmark",
+                PeriodCount = 10,
+                PeriodDivision = 10,
+                Profile = GeometricPropertyModel.CircularBeamProfileWithoutThickness
+            };
+
+        public static BeamRequest<RectangularProfile> RectangularBeam
+            => new BeamRequest<RectangularProfile>
+            {
+                AngularFrequencyStep = 1,
+                Author = "Darth Vader",
+                Fastenings = new List<Fastening>
+                    {
+                        new Fastening
+                        {
+                            NodePosition = 0,
+                            Type = "Pinned"
+                        },
+                        new Fastening
+                        {
+                            NodePosition = 2,
+                            Type = "Pinned"
+                        }
+                    },
+                Forces = new List<Force>
+                    {
+                        new Force
+                        {
+                            NodePosition = 1,
+                            Value = 10
+                        }
+                    },
+                ForceType = "Harmonic",
+                InitialAngularFrequency = 0,
+                FinalAngularFrequency = 10,
+                Length = 1,
+                Material = "Aluminum",
+                NumberOfElements = 2,
+                NumericalMethod = "Newmark",
+                PeriodCount = 10,
+                PeriodDivision = 10,
+                Profile = GeometricPropertyModel.RectangularBeamProfileWithoutThickness
+            };
+    }
+}
